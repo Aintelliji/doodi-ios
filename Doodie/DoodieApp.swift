387a676048db6343cc131cd7ece23a7d5b22464e
Int-TRUE <dmg06017@naver.com>
2025-10-11 14:44:41 +0900

    Initial Commit

---
+++ Doodie/DoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoodieApp.swift
+//  Doodie
+//
+//  Created by 수진 on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
