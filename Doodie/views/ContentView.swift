def57ff6a82d6764ee67047ae594f671454e745b
Int-TRUE <dmg06017@naver.com>
2025-10-11 15:12:36 +0900

    add home view, login view files

---
+++ Doodie/views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Doodie
+//
+//  Created by 수진 on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // 회원 관련 뷰모델
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
