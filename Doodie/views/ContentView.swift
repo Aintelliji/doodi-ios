94266400cf25380998b74b6cb82a70493d95e8df
Int-TRUE <dmg06017@naver.com>
2025-10-11 15:13:49 +0900

    분기처리

--- Doodie/views/ContentView.swift
+++ Doodie/views/ContentView.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct ContentView: View {
     
     // 회원 관련 뷰모델
+    // 임시
+    var isLogin = true
+    
     
     var body: some View {
-        
+        if (isLogin) {
+            HomeView()
+        }else{
+            LoginView()
+        }
     }
 }
 
