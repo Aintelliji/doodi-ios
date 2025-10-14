9c9cb9245179d55bc14d2b7a137d7f3c38a80e6c
Int-TRUE <dmg06017@naver.com>
2025-10-15 01:49:11 +0900

    하 몰라

---
+++ Doodie/model/ChatMessageDto.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessageDto.swift
+//  Doodie
+//
+//  Created by 수진 on 10/15/25.
+//
+
+import SwiftUI
+
+struct ChatMessageDto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatMessageDto()
+}
