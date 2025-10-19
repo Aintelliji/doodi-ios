1058e604801812a89b0239dce3ff9227b8bd0e5b
Int-TRUE <dmg06017@naver.com>
2025-10-19 17:34:00 +0900

    첫번째 네비게이션 뜯어고치기 성공...

---
+++ Doodie/model/ResultDto.swift
@@ -0,0 +1,18 @@
+//
+//  ResultDto.swift
+//  Doodie
+//
+//  Created by 수진 on 10/19/25.
+//
+
+import SwiftUI
+
+struct ResultDto: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultDto()
+}
